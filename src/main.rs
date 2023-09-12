53f206d13a879c7ff2b56225b0f2a00d4545d63e
SapphoTech <144105693+SapphoTech@users.noreply.github.com>
2023-09-11 23:15:22 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use crate::web::get_all_routes;
 use integra::core::router::{ServiceWithRouter};
 use std::sync::Arc;
 use hyper::service::make_service_fn;
+use std::env;
 
 use once_cell::sync::Lazy;
 use std::sync::Mutex;
@@ -35,7 +36,9 @@ async fn main() {
         async move { Ok::<_, Infallible>(service) }
     });
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    let ip = env::var("IP").unwrap_or_else(|_| String::from("0.0.0.0"));
+    let port = env::var("PORT").unwrap_or_else(|_| String::from("3000"));
+    let addr = format!("{}:{}", ip, port).parse::<SocketAddr>().unwrap();
     let server = Server::bind(&addr).serve(make_svc);
 
     if let Err(e) = server.await {
